19675ce68d17151c396fa19886096d4c50ce60ee
Åke Engelbrektson <eson57@users.noreply.github.com>
2018-12-10 11:08:04 +0100

    Create harbour-nextcloudnotes-sv.ts
    Swedish translation if wanted

---
+++ translations/harbour-nextcloudnotes-sv.ts
@@ -0,0 +1,393 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Nextcloud Notes</source>
+        <translation>Nextcloud Notes</translation>
+    </message>
+    <message>
+        <source>for Sailfish OS</source>
+        <translation>för Sailfish OS</translation>
+    </message>
+    <message>
+        <source>Notes</source>
+        <translation>Anteckningar</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;This app is released under the MIT license.&lt;/p&gt;&lt;p&gt;The source code is available on &lt;a href=&quot;https://github.com/scharel/harbour-nextcloudnotes&quot;&gt;GitHub&lt;/a&gt;.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Denna app är släppt under MIT-licensen.&lt;/p&gt;&lt;p&gt;Källkoden finns på &lt;a href=&quot;https://github.com/scharel/harbour-nextcloudnotes&quot;&gt;GitHub&lt;/a&gt;.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>MIT License</source>
+        <translation>MIT License</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;This app uses &lt;a href=&quot;https://github.com/showdownjs/showdown/tree/1.9.0&quot;&gt;ShowdownJS v1.9&lt;/a&gt; to convert Markdown to HTML.&lt;/p&gt;&lt;p&gt;Showdown 1.x is released under GPL 2.0.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Denna app använder &lt;a href=&quot;https://github.com/showdownjs/showdown/tree/1.9.0&quot;&gt;ShowdownJS v1.9&lt;/a&gt; för att konvertera Markdown till HTML.&lt;/p&gt;&lt;p&gt;Showdown 1.x är släppt under GPL 2.0.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>GPL 2.0 License</source>
+        <translation>GPL 2.0 License</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Notes</source>
+        <translation>Anteckningar</translation>
+    </message>
+</context>
+<context>
+    <name>EditPage</name>
+    <message>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Återställ</translation>
+    </message>
+    <message>
+        <source>Markdown syntax</source>
+        <translation>Markdown syntax</translation>
+    </message>
+    <message>
+        <source>No category</source>
+        <translation>Ingen kategori</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Ändrad</translation>
+    </message>
+</context>
+<context>
+    <name>GPLLicense</name>
+    <message>
+        <source>GPL 2.0 License</source>
+        <translation>GPL 2.0 License</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <source>Login</source>
+        <translation>Logga in</translation>
+    </message>
+    <message>
+        <source>(starting with &quot;https://&quot;)</source>
+        <translation>(börjar med &quot;https://&quot;)</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Användarnamn</translation>
+    </message>
+    <message>
+        <source>Nextcloud server</source>
+        <translation>Nextcloud server</translation>
+    </message>
+    <message>
+        <source>Account name</source>
+        <translation>Kontonamn</translation>
+    </message>
+    <message>
+        <source>Security</source>
+        <translation>Säkerhet</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&lt;strong&gt;CAUTION: Your password will be saved without any encryption on the device!&lt;/strong&gt;&lt;br&gt;Please consider creating a dedicated app password! Open your Nextcloud in a browser and go to &lt;i&gt;Settings&lt;/i&gt; → &lt;i&gt;Security&lt;/i&gt;.</source>
+        <translation>&lt;strong&gt;OBS! Ditt lösenord kommer att sparas okrypterat på enheten!&lt;/strong&gt;&lt;br&gt;Överväg att skapa ett dedikerat app-lösenord! Öppna din Nextcloud i en webbläsare och gå till &lt;i&gt;Inställningar&lt;/i&gt; → &lt;i&gt;Säkerhet&lt;/i&gt;.</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Lösenord</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+</context>
+<context>
+    <name>MITLicense</name>
+    <message>
+        <source>MIT License</source>
+        <translation>MIT License</translation>
+    </message>
+</context>
+<context>
+    <name>NotePage</name>
+    <message>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <source>Notes</source>
+        <translation>Anteckningar</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>Updating...</source>
+        <translation>Uppdaterar...</translation>
+    </message>
+    <message>
+        <source>Last update</source>
+        <translation>Senaste uppdatering</translation>
+    </message>
+    <message>
+        <source>never</source>
+        <translation>aldrig</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Ändrad</translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <source>No category</source>
+        <translation>Ingen kategori</translation>
+    </message>
+</context>
+<context>
+    <name>NotesApi</name>
+    <message>
+        <source>Today</source>
+        <translation>I dag</translation>
+    </message>
+    <message>
+        <source>Yesterday</source>
+        <translation>I går</translation>
+    </message>
+</context>
+<context>
+    <name>NotesPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Add note</source>
+        <translation>Lägg till anteckning</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>Updating...</source>
+        <translation>Uppdaterar...</translation>
+    </message>
+    <message>
+        <source>Last update</source>
+        <translation>Senaste uppdatering</translation>
+    </message>
+    <message>
+        <source>never</source>
+        <translation>aldrig</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Ändrad</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Deleting note</source>
+        <translation>Tar bort anteckning</translation>
+    </message>
+    <message>
+        <source>No account yet</source>
+        <translation>Inget konto ännu</translation>
+    </message>
+    <message>
+        <source>Got to the settings to add an account</source>
+        <translation>Gå till inställningarna för att lägga till ett konto</translation>
+    </message>
+    <message>
+        <source>No notes yet</source>
+        <translation>Inga anteckningar ännu</translation>
+    </message>
+    <message>
+        <source>Pull down to add a note</source>
+        <translation>Dra neråt för att lägga till anteckning</translation>
+    </message>
+    <message>
+        <source>No result</source>
+        <translation>Inget resultat</translation>
+    </message>
+    <message>
+        <source>Try another query</source>
+        <translation>Försök med en annan söksträng</translation>
+    </message>
+    <message>
+        <source>An error occurred</source>
+        <translation>Ett fel inträffade</translation>
+    </message>
+    <message>
+        <source>Open the settings to configure your Nextcloud accounts</source>
+        <translation>Öppna inställningarna för att konfigurera dina Nextcloud-konton</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Accounts</source>
+        <translation>Konton</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>No Nextcloud account yet</source>
+        <translation>Inget Nextcloud-konto ännu</translation>
+    </message>
+    <message>
+        <source>Add account</source>
+        <translation>Lägg till konto</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <source>Unnamed account</source>
+        <translation>Namnlöst konto</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Deleting account</source>
+        <translation>Tar bort konto</translation>
+    </message>
+    <message>
+        <source>Synchronization</source>
+        <translation>Synkronisering</translation>
+    </message>
+    <message>
+        <source>Auto-Sync</source>
+        <translation>Autosynk</translation>
+    </message>
+    <message>
+        <source>Periodically pull notes from the server</source>
+        <translation>Hämta anteckningar från servern periodiskt</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Inaktiverad</translation>
+    </message>
+    <message>
+        <source>Minutes</source>
+        <translation>Minuter</translation>
+    </message>
+    <message>
+        <source>Seconds</source>
+        <translation>Sekunder</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Utseende</translation>
+    </message>
+    <message>
+        <source>Show separator</source>
+        <translation>Visa avdelare</translation>
+    </message>
+    <message>
+        <source>Show a separator line between the notes</source>
+        <translation>Visa en separatorlinje mellan anteckningarna</translation>
+    </message>
+    <message>
+        <source>lines</source>
+        <translation>rader</translation>
+    </message>
+    <message>
+        <source>every</source>
+        <translation>varje</translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <source>Title alphabetically</source>
+        <translation>Titlar alfabetiskt</translation>
+    </message>
+    <message>
+        <source>Sort notes by</source>
+        <translation>Sortera anteckningar efter</translation>
+    </message>
+    <message>
+        <source>Last edited</source>
+        <translation>Senast redigerad</translation>
+    </message>
+    <message>
+        <source>This will also change how the notes are grouped</source>
+        <translation>Detta ändrar också hur anteckningarna grupperas</translation>
+    </message>
+    <message>
+        <source>Number of lines in the preview</source>
+        <translation>Antal rader i förhandsgranskningen</translation>
+    </message>
+    <message>
+        <source>Editing</source>
+        <translation>Redigering</translation>
+    </message>
+    <message>
+        <source>Monospaced font</source>
+        <translation>Monospace teckensnitt</translation>
+    </message>
+    <message>
+        <source>Use a monospeced font to edit a note</source>
+        <translation>Använd ett monospace teckensnitt för redigering</translation>
+    </message>
+    <message>
+        <source>Capital &apos;X&apos; in checkboxes</source>
+        <translation>Stort &quot;X&quot; i kryssrutor</translation>
+    </message>
+    <message>
+        <source>For interoperability with other apps such as Joplin</source>
+        <translation>För samverkan med andra program som Joplin</translation>
+    </message>
+    <message>
+        <source>unknown</source>
+        <translation>okänd</translation>
+    </message>
+</context>
+<context>
+    <name>UnencryptedDialog</name>
+    <message>
+        <source>I do understand</source>
+        <translation>Jag förstår</translation>
+    </message>
+    <message>
+        <source>&lt;strong&gt;Your username and password will be transferred unencrypted over the network when you enable this option.&lt;br&gt;Do not accept unless you know exactly what you are doing!&lt;/strong   &gt;&lt;br&gt;&lt;a href=&quot;https://github.com/nextcloud/notes/wiki/API-0.2&quot;&gt;More information...&lt;/a&gt;</source>
+        <translation>&lt;strong&gt;Ditt användarnamn och lösenord överförs okrypterat över nätverket när du aktiverar det här alternativet.&lt;br&gt;Acceptera inte, såvida du inte vet exakt vad håller på med!&lt;/strong   &gt;&lt;br&gt;&lt;a href=&quot;https://github.com/nextcloud/notes/wiki/API-0.2&quot;&gt;Mer information...&lt;/a&gt;</translation>
+    </message>
+</context>
+</TS>
